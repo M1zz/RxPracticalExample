c7b980ccc48a19c54200e9b3a251504cbaa10df0
hyunho.lee <hyunho.lee@wadiz.kr>
2021-04-12 23:24:59 +0900

    first commit

---
+++ RxPracticalExample/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  RxPracticalExample
+//
+//  Created by 이현호 on 2021/04/12.
+//
+
+import UIKit
+
+class AppCoordinator {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
---
+++ RxPracticalExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxPracticalExample
+//
+//  Created by 이현호 on 2021/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
